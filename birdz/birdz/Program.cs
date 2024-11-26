ae0f7153bfee6f1c54a6bbb21cfc06efd1371ac5
rakthai2549 <rakthai2549@gmail.com>
2024-11-26 11:31:23 +0700

    g1

---
+++ birdz/birdz/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "My name is kankacee hongsawong";
+string student_id = "My student id: 673450185-7"; 
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is kankacee hongsawong"+name);
+Console.WriteLine("My student id: 673450185-7"+student_id);
